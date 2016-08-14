af8d823023172ddf7d43367a1e8211840784871c
Tad <terry.donaghe@gmail.com>
2016-08-14 09:19:20 -0700

    Updated code to properly generate hash

--- index.js
+++ index.js
@@ -11,35 +11,29 @@ const publicKey = 'b958258344feb4298d7a2da3af4007eb';
 
 getPrivateKey()
   .then(
-    privateKey => {
-      getHash(getTimeStamp(), privateKey, publicKey).then(
-        hash => {
-          console.log(hash);
-        }
-      );
-    },
-    err => {
-      console.log('Unable to read private.txt file.');
-    }
-);
+    privateKey =>
+      getHash(getTimeStamp(), privateKey, publicKey).then(hash => console.log(hash)),
+    err =>
+      console.log('Unable to read private.txt file.'));
 
 function getTimeStamp(){
-  return Date.now();
+  let ts = Date.now();
+  console.log(ts);
+  return ts;
 }
 
 function getHash(timeStamp, privateKey, publicKey) {
-  return new Promise((resolve,reject) => {
-    resolve(crypto.createHash('md5').update(timeStamp + privateKey + publicKey).digest('hex'));
-  });
+  return new Promise((resolve,reject) =>
+    resolve(crypto.createHash('md5').update(timeStamp + privateKey + publicKey).digest('hex')));
 }
 
 function getPrivateKey(){
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) =>
     fs.readFile('private.txt', 'utf8', (err, data) => {
       if(!err && data)
-        resolve(data);
+        resolve(data.trim())
       else
         reject(err);
-    });
-  });
+    })
+  );
 }
